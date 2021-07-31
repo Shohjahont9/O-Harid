86e19c5dba0a9094eace8dc48d2995de27312ead
Shohjahont9 <Shohjahont9@gmail.com>
2021-07-31 16:48:40 +0500

    update

--- app/src/main/java/lars_lion/dev/o_harid/ui/book/BookFragment.kt
+++ app/src/main/java/lars_lion/dev/o_harid/ui/book/BookFragment.kt
@@ -1,14 +1,12 @@
 package lars_lion.dev.o_harid.ui.book
 
 import android.os.Bundle
-import android.os.Environment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.koushikdutta.ion.Ion
 import dagger.hilt.android.AndroidEntryPoint
 import lars_lion.dev.o_harid.R
 import lars_lion.dev.o_harid.adapter.PaidBooksAdapter
@@ -19,14 +17,13 @@ import lars_lion.dev.o_harid.network.response.search.Object
 import lars_lion.dev.o_harid.preferences.PreferencesManager
 import lars_lion.dev.o_harid.ui.main.MainViewModel
 import lars_lion.dev.o_harid.utils.*
-import java.io.*
 import javax.inject.Inject
 
 
 @AndroidEntryPoint
 class BookFragment : BaseFragment<FragmentBookBinding>(),
     PaidBooksAdapter.BestSellerAdapterListener, SearchBooksAdapter.BestSellerAdapterListener,
-    androidx.appcompat.widget.SearchView.OnQueryTextListener  {
+    androidx.appcompat.widget.SearchView.OnQueryTextListener {
 
     lateinit var paidBooksAdapter: PaidBooksAdapter
 
@@ -35,7 +32,6 @@ class BookFragment : BaseFragment<FragmentBookBinding>(),
     val viewModelMain: MainViewModel by viewModels()
     lateinit var searchAdapter: SearchBooksAdapter
     val viewModel: BookViewModel by viewModels()
-
 
     override fun setBinding(inflater: LayoutInflater, container: ViewGroup?): FragmentBookBinding =
         FragmentBookBinding.inflate(layoutInflater)
@@ -56,7 +52,7 @@ class BookFragment : BaseFragment<FragmentBookBinding>(),
             hideKeyBoard(it)
         }
 
-        binding!!.rvBook.setOnClickListener { it->
+        binding!!.rvBook.setOnClickListener { it ->
             binding!!.etSearchPlaces.clearFocus()
             hideKeyBoard(it)
         }
@@ -113,17 +109,7 @@ class BookFragment : BaseFragment<FragmentBookBinding>(),
         data: lars_lion.dev.o_harid.network.response.paidBooks.Object
     ) {
         toast(position.toString())
-        Ion.with(context)
-            .load(data.file) // have a ProgressBar get updated automatically with the percent
-//            .progressBar(progressBar) // and a ProgressDialog
-//            .progressDialog(progressDialog) // can also use a custom callback
-//            .progress { downloaded, total -> println("$downloaded / $total") }
-            .write(File(  Environment.getExternalStorageDirectory(), "MyProfile"))
-            .setCallback { e, file ->
-                toast("download completed")
-                // download done...
-                // do stuff with the File or error
-            }
+
     }
 
     override fun onDeleteItem(
@@ -145,8 +131,7 @@ class BookFragment : BaseFragment<FragmentBookBinding>(),
     private fun initRvSearch() {
         searchAdapter = SearchBooksAdapter(this)
         with(binding!!.rvSearch) {
-            layoutManager =
-                LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+            layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
             setHasFixedSize(true)
         }
     }
--- app/src/main/java/lars_lion/dev/o_harid/ui/book_detail/BookDetailFragment.kt
+++ app/src/main/java/lars_lion/dev/o_harid/ui/book_detail/BookDetailFragment.kt
@@ -1,20 +1,21 @@
 package lars_lion.dev.o_harid.ui.book_detail
 
+import android.Manifest.permission.READ_EXTERNAL_STORAGE
 import android.annotation.SuppressLint
 import android.content.Context
+import android.content.pm.PackageManager
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.ViewTreeObserver
 import android.widget.TextView
 import androidx.activity.OnBackPressedCallback
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import coil.load
 import coil.request.ImageRequest
 import coil.request.ImageResult
@@ -22,27 +23,28 @@ import com.airbnb.lottie.LottieAnimationView
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.card.MaterialCardView
+import com.sasank.roundedhorizontalprogress.RoundedHorizontalProgressBar
+import com.tonyodev.fetch2.*
+import com.tonyodev.fetch2core.DownloadBlock
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_login.*
 import lars_lion.dev.o_harid.R
-import lars_lion.dev.o_harid.adapter.BestSellerAdapter
 import lars_lion.dev.o_harid.adapter.CommentsAdapter
-import lars_lion.dev.o_harid.adapter.JanrAdapter
 import lars_lion.dev.o_harid.base.BaseFragment
 import lars_lion.dev.o_harid.databinding.FragmentBookDetailBinding
 import lars_lion.dev.o_harid.model.Comment
-import lars_lion.dev.o_harid.network.response.bookType.Object
 import lars_lion.dev.o_harid.preferences.PreferencesManager
-import lars_lion.dev.o_harid.ui.main.MainViewModel
 import lars_lion.dev.o_harid.utils.*
-import org.w3c.dom.Text
+import java.io.File
 import javax.inject.Inject
+
 
 @AndroidEntryPoint
 class BookDetailFragment : BaseFragment<FragmentBookDetailBinding>(),
     CommentsAdapter.BestSellerAdapterListener {
 
     lateinit var commentsAdapter: CommentsAdapter
+    var url = ""
 
     @Inject
     lateinit var prefs: PreferencesManager
@@ -72,6 +74,7 @@ class BookDetailFragment : BaseFragment<FragmentBookDetailBinding>(),
         loadBookItems()
 
         onClicks()
+
     }
 
     private fun onClicks() {
@@ -80,7 +83,6 @@ class BookDetailFragment : BaseFragment<FragmentBookDetailBinding>(),
             cvBack.setOnClickListener {
                 findNavController().navigateSafe(R.id.action_bookDetailFragment_to_mainFragment)
             }
-
             cvLib.setOnClickListener {
                 viewModel.addFavBook(prefs.bookId)
                 viewModel.addFavouriteBook.observe(viewLifecycleOwner, EventObserver {
@@ -129,9 +131,9 @@ class BookDetailFragment : BaseFragment<FragmentBookDetailBinding>(),
                     is UiState.Success -> {
                         progress.visible(false)
                         dialog.dismiss()
-                        toast("Bu kitob sotib olingan kitoblar ro`yhatiga kiritildi!")
-                        findNavController().navigateSafe(R.id.action_bookDetailFragment_to_bookFragment)
 
+                        if (isAlreadyPermission()) downloadBook()
+                        else checkPersmission()
                     }
                     is UiState.Error -> {
                         progress.visible(false)
@@ -147,6 +149,155 @@ class BookDetailFragment : BaseFragment<FragmentBookDetailBinding>(),
 
     }
 
+    private fun isAlreadyPermission(): Boolean {
+        return ContextCompat.checkSelfPermission(
+            requireContext(),
+            READ_EXTERNAL_STORAGE
+        ) == PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(
+            requireContext(),
+            android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun checkPersmission() {
+        if (ContextCompat.checkSelfPermission(
+                requireContext(),
+                android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+            ) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(
+                requireContext(),
+                READ_EXTERNAL_STORAGE
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    requireActivity(),
+                    android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+                ) && ActivityCompat.shouldShowRequestPermissionRationale(
+                    requireActivity(),
+                    READ_EXTERNAL_STORAGE
+                )
+            ) {
+
+            } else {
+                ActivityCompat.requestPermissions(
+                    requireActivity(),
+                    listOf<String>(
+                        android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                        READ_EXTERNAL_STORAGE
+                    ).toTypedArray(), PERMISSION_READ_AND_WRITE_EXTERNAL_STORAGE
+                )
+            }
+        } else {
+            downloadBook()
+        }
+    }
+
+    private fun downloadBook() {
+        with(binding!!) {
+            tvPrice.text = ""
+            tvProgress.visible(true)
+            horizontalProgress.visible(true)
+
+            downloadFile(url, horizontalProgress, tvProgress)
+        }
+    }
+
+    var file: File? = null
+    private fun downloadFile(url: String, progressBar: RoundedHorizontalProgressBar, textView: TextView) {
+        println("url -> $url")
+        val config = FetchConfiguration.Builder(context = requireContext()).build()
+        val downloader = Fetch.Impl.getInstance(config)
+        file = File(requireContext().getExternalFilesDir(null), "." + "O-Harid$url/$url")
+        val request = Request(url, file!!.absolutePath)
+        request.priority = Priority.HIGH
+        request.networkType = NetworkType.ALL
+        downloader.enqueue(request, { updatedRequest ->
+            println("Download success -> ${updatedRequest.fileUri}")
+            toast("Yuklash boshlandi.")
+        }) { error ->
+            println("Download error -> ${error.throwable}")
+        }
+        downloader.addListener(downloaderListener(progressBar, textView))
+    }
+
+    private fun downloaderListener(progressBar: RoundedHorizontalProgressBar, textView:TextView): FetchListener {
+        return object : FetchListener {
+            override fun onAdded(download: Download) {
+                println("Download method -> onAdded")
+            }
+
+            override fun onCancelled(download: Download) {
+                println("Download method -> onCancelled")
+            }
+
+            override fun onCompleted(download: Download) {
+                if (file != null && file!!.exists()) {
+//                    encryptedFile(file!!.absolutePath)
+//                    viewModel.updateDownloadedUrl(mUuid, file!!.absolutePath)
+                    toast("Download Successfully")
+//                    toast("Bu kitob sotib olingan kitoblar ro`yhatiga kiritildi!")
+                    findNavController().navigateSafe(R.id.action_bookDetailFragment_to_bookFragment)
+                    file = null
+                }
+            }
+
+            override fun onDeleted(download: Download) {
+                println("Download method -> onDeleted")
+            }
+
+            override fun onDownloadBlockUpdated(
+                download: Download,
+                downloadBlock: DownloadBlock,
+                totalBlocks: Int
+            ) {
+                println("Download method -> onDownloadBlockUpdated")
+
+            }
+
+            override fun onError(download: Download, error: Error, throwable: Throwable?) {
+                println("Download method -> onError")
+            }
+
+            override fun onPaused(download: Download) {
+                println("Download method -> onPaused")
+            }
+
+            override fun onProgress(
+                download: Download,
+                etaInMilliSeconds: Long,
+                downloadedBytesPerSecond: Long
+            ) {
+                println("Download method -> onProgress")
+                println("downloadedBytesPerSecond -> $downloadedBytesPerSecond Mb/s")
+                progressBar.progress = download.progress
+                textView.text = "${download.progress}%"
+            }
+
+            override fun onQueued(download: Download, waitingOnNetwork: Boolean) {
+                println("Download method -> onQueued")
+            }
+
+            override fun onRemoved(download: Download) {
+                println("Download method -> onRemoved")
+            }
+
+            override fun onResumed(download: Download) {
+                println("Download method -> onResumed")
+            }
+
+            override fun onStarted(
+                download: Download,
+                downloadBlocks: List<DownloadBlock>,
+                totalBlocks: Int
+            ) {
+                println("Download method -> onStarted")
+            }
+
+            override fun onWaitingNetwork(download: Download) {
+                println("Download method -> onWaitingNetwork")
+            }
+        }
+    }
+
     @SuppressLint("SetTextI18n")
     private fun loadBookItems() {
         viewModel.getBookDetail(prefs.bookId)
@@ -157,6 +308,8 @@ class BookDetailFragment : BaseFragment<FragmentBookDetailBinding>(),
                 is UiState.Success -> {
                     val data = it.value.`object`
                     println("data-> $data")
+                    url = data.file
+
                     binding!!.tvPrice.text = "${data.price} so`m"
                     binding!!.tvAuthor.text = data.author
                     binding!!.tvTitle.text = data.name
@@ -256,5 +409,10 @@ class BookDetailFragment : BaseFragment<FragmentBookDetailBinding>(),
 
     override fun onItemClick(position: Int, data: Comment) {
 
+    }
+
+
+    companion object {
+        private val PERMISSION_READ_AND_WRITE_EXTERNAL_STORAGE = 1001
     }
 }
